267e9f5b2e045e9e2206903276bddaddda2d09ab
Andrew Herbert <andrewh42@users.noreply.github.com>
2020-05-03 12:25:50 +1000

    fix flakey motion model tests

--- test/shutter-model.test.js
+++ test/shutter-model.test.js
@@ -8,13 +8,16 @@ const { MotionModel } = require('../lib/motion');
 test('upFully', function (assert) {
 	assert.plan(4);
 
+	const now = 1893448800000;
+	timekeeper.freeze(new Date(now));
+
 	const shutter = new MotionModel(30000);
 
 	assert.equal(shutter.getCurrentPosition(), 0);
 	shutter.setCurrentState('INCREASING');
 	assert.equal(shutter.getCurrentState(), 'INCREASING');
 
-	timekeeper.travel(new Date(Date.now() + 30000));
+	timekeeper.travel(new Date(now + 30000));
 	assert.equal(shutter.getCurrentState(), 'STOPPED');
 	assert.equal(shutter.getCurrentPosition(), 100);
 
@@ -26,13 +29,16 @@ test('upFully', function (assert) {
 test('upPartial', function (assert) {
 	assert.plan(4);
 
+	const now = 1893448800000;
+	timekeeper.freeze(new Date(now));
+
 	const shutter = new MotionModel(30000);
 
 	assert.equal(shutter.getCurrentPosition(), 0);
 	shutter.setCurrentState('INCREASING');
 	assert.equal(shutter.getCurrentState(), 'INCREASING');
 
-	timekeeper.travel(new Date(Date.now() + 15000));
+	timekeeper.travel(new Date(now + 15000));
 	assert.equal(shutter.getCurrentState(), 'INCREASING');
 	assert.equal(shutter.getCurrentPosition(), 50);
 
@@ -44,13 +50,16 @@ test('upPartial', function (assert) {
 test('upStop', function (assert) {
 	assert.plan(4);
 
+	const now = 1893448800000;
+	timekeeper.freeze(new Date(now));
+
 	const shutter = new MotionModel(30000);
 
 	assert.equal(shutter.getCurrentPosition(), 0);
 	shutter.setCurrentState('INCREASING');
 	assert.equal(shutter.getCurrentState(), 'INCREASING');
 
-	timekeeper.travel(new Date(Date.now() + 15000));
+	timekeeper.travel(new Date(now + 15000));
 	shutter.setCurrentState('STOPPED');
 
 	assert.equal(shutter.getCurrentState(), 'STOPPED');
@@ -64,6 +73,9 @@ test('upStop', function (assert) {
 test('downFully', function (assert) {
 	assert.plan(4);
 
+	const now = 1893448800000;
+	timekeeper.freeze(new Date(now));
+
 	const shutter = new MotionModel(30000);
 
 	shutter.setCurrentPosition(100);
@@ -71,7 +83,7 @@ test('downFully', function (assert) {
 	shutter.setCurrentState('DECREASING');
 	assert.equal(shutter.getCurrentState(), 'DECREASING');
 
-	timekeeper.travel(new Date(Date.now() + 30000));
+	timekeeper.travel(new Date(now + 30000));
 	assert.equal(shutter.getCurrentState(), 'STOPPED');
 	assert.equal(shutter.getCurrentPosition(), 0);
 
@@ -83,6 +95,9 @@ test('downFully', function (assert) {
 test('downStop', function (assert) {
 	assert.plan(4);
 
+	const now = 1893448800000;
+	timekeeper.freeze(new Date(now));
+
 	const shutter = new MotionModel(30000);
 
 	shutter.setCurrentPosition(100);
@@ -90,7 +105,7 @@ test('downStop', function (assert) {
 	shutter.setCurrentState('DECREASING');
 	assert.equal(shutter.getCurrentState(), 'DECREASING');
 
-	timekeeper.travel(new Date(Date.now() + 15000));
+	timekeeper.travel(new Date(now + 15000));
 	shutter.setCurrentState('STOPPED');
 
 	assert.equal(shutter.getCurrentState(), 'STOPPED');
